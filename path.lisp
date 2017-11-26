d2d9dad4d03f104b7b8590179b04aaa08144a053
baggers <baggers@cave>
2017-11-26 17:09:47 +0100

    structified

--- path.lisp
+++ path.lisp
@@ -16,101 +16,73 @@
 (deftype tiny-path-index ()
   '(unsigned-byte 16))
 
+;;------------------------------------------------------------
+
 (defstruct (tiny-path
              (:constructor %make-tiny-path))
   (nodes nil :type list :read-only t)
   ;; these 2 slots are an optimization so we can avoid list walking
-  (absolute nil :type boolean :read-only t)
+  (absolute-p nil :type boolean :read-only t)
   (length 0 :type tiny-path-index :read-only t))
 
-;; (defclass path ()
-;;   ;; nodes are stored in reverse order
-;;   ((nodes :initform nil :initarg :nodes)
-;;    (absolute :initarg :absolute :initform nil :reader absolute-path-p)
-;;    (len :initarg :len :reader path-length)))
 
 (defstruct tiny-path-node
-  (name nil :type symbol :read-only t)
-  (terminal nil :type boolean :read-only t))
+  (name nil :type string :read-only t)
+  (terminal-p nil :type boolean :read-only t))
 
-;; (defclass node ()
-;;   ((name :initarg :name :reader node-name)
-;;    (terminal :reader terminal-node-p)))
 
 (defstruct (tiny-path-file
+             (:constructor %make-tiny-path-file)
              (:include tiny-path-node
-                       (terminal t :type boolean :read-only t))))
+                       (terminal-p t :type boolean :read-only t))))
 
-;; (defclass file (node)
-;;   ((terminal :initform t)))
 
 (defstruct (tiny-path-dir
+             (:constructor %make-tiny-path-dir)
              (:include tiny-path-node
-                       (terminal nil :type boolean :read-only t)))
+                       (terminal-p nil :type boolean :read-only t)))
   (seperator "/" :type string :read-only t))
 
-;; (defclass dir (node)
-;;   ((terminal :initform nil)
-;;    (seperator :initform "/")))
 
 (defstruct (tiny-path-root-dir
-             (:include tiny-path-node
-                       (terminal nil :type boolean :read-only t)))
-  (absolute-prefix "/" :type string :read-only t))
-
-;; (defclass root-dir (dir)
-;;   ((terminal :initform nil)
-;;    (absolute-prefix :initform "/")))
+             (:constructor %make-tiny-path-root-dir)
+             (:include tiny-path-dir
+                       (name "" :type string :read-only t))))
 
 ;;------------------------------------------------------------
 
-(defmethod print-object ((dir dir) stream)
-  (with-slots (name seperator) dir
-    (if *in-print-path*
-        (format stream "~a~a" name
-                (if *in-render-path*
-                    seperator
-                    "/"))
-        (format stream "#<DIR ~s>" name))))
+(defmethod print-object ((dir tiny-path-dir) stream)
+  (if *in-print-path*
+      (format stream "~a~a" (tiny-path-dir-name dir)
+              (if *in-render-path*
+                  (tiny-path-dir-seperator dir)
+                  "/"))
+      (format stream "#<DIR \"~a\">" (tiny-path-dir-name dir))))
 
-(defmethod print-object ((dir root-dir) stream)
-  (with-slots (name seperator absolute-prefix) dir
-    (if *in-print-path*
-        (format stream "~a~a~a" name
-                (if *in-render-path*
-                    absolute-prefix
-                    "/")
-                (if *in-render-path*
-                    seperator
-                    "/"))
-        (format stream "#<DIR ~s>" name))))
+(defmethod print-object ((file tiny-path-file) stream)
+  (if *in-print-path*
+      (format stream (tiny-path-file-name file))
+      (format stream "#<FILE \"~a\">" (tiny-path-file-name file))))
 
-(defmethod print-object ((file file) stream)
-  (with-slots (name) file
-    (if *in-print-path*
-        (format stream name)
-        (format stream "#<FILE ~s>" name))))
-
-(defmethod print-object ((path path) stream)
-  (with-slots (nodes) path
-    (let ((*in-print-path* t))
-      (format stream "#<PATH \"~{~s~}\">" (reverse nodes)))))
+(defmethod print-object ((path tiny-path) stream)
+  (let ((*in-print-path* t)
+        (*print-escape* nil))
+    (format stream "#<PATH \"~{~a~}\">" (reverse (tiny-path-nodes path)))))
 
 (defun render-path (path &optional stream)
-  (with-slots (nodes) path
-    (let ((*in-print-path* t)
-          (*in-render-path* t))
-      (format stream "~{~s~}" (reverse nodes)))))
+  (let ((*in-print-path* t)
+        (*in-render-path* t))
+    (format stream "~{~a~}" (reverse (tiny-path-nodes path)))))
 
 ;;------------------------------------------------------------
 
 (defun make-path-from-nodes (nodes)
   ;; What people extending tiny path will call from their constructors
-  (assert (not-some #'root-dir-p (butlast nodes)) ()
+  (assert (notany #'tiny-path-root-dir-p (butlast nodes)) ()
           "Only the last node in the node list may be a root-dir")
-  (make-instance 'path :nodes nodes
-                 :absolute (typep (car (last nodes)) 'root-dir)
-                 :len (length nodes)))
+  (%make-tiny-path :nodes nodes
+                   :absolute-p (typep (car (last nodes)) 'tiny-path-root-dir)
+                   :length (length nodes)))
 
 (defun make-path (str &optional dir)
   (let ((split (split-string str :separator '(#\/))))
@@ -123,32 +95,35 @@
                              (rest sub-strs)
                              (error "invalid trailing seperator"))
                          sub-strs))
-           (nodes (mapcar (lambda (x) (make-instance 'dir :name x))
+           (nodes (mapcar (lambda (x) (%make-tiny-path-dir :name x))
                           (rest sub-strs)))
            (nodes (cons (if dir
-                            (make-instance 'dir :name (first sub-strs))
-                            (make-instance 'file :name (first sub-strs)))
+                            (%make-tiny-path-dir :name (first sub-strs))
+                            (%make-tiny-path-file :name (first sub-strs)))
                         nodes)))
       (make-path-from-nodes nodes))))
 
 ;;------------------------------------------------------------
 
 (defun path+ (a b)
-  (with-slots ((nodes-a nodes)
-               (len-a len)) a
-    (assert (not (terminal-node-p (first nodes-a))))
-    (with-slots ((nodes-b nodes)
-                 (len-b len)) b
-      (make-instance 'path :nodes (append nodes-b nodes-a)
-                     :absolute (absolute-path-p a)
-                     :len (+ len-a len-b)))))
+  (let ((nodes-a (tiny-path-nodes a))
+        (nodes-b (tiny-path-nodes b))
+        (len-a (tiny-path-length a))
+        (len-b (tiny-path-length b)))
+    (assert (not (tiny-path-node-terminal-p (first nodes-a))))
+    (%make-tiny-path :nodes (append nodes-b nodes-a)
+                     :absolute-p (tiny-path-absolute-p a)
+                     :length (+ len-a len-b))))
 
 (defun path-pop (path &optional (n 1))
   (assert (>= n 1) () "Can't pop less that 1 node from path")
-  (with-slots (nodes len) path
-    (values (make-instance 'path :nodes (nthcdr n nodes)
-                           :absolute (absolute-path-p path)
-                           :len (- len n))
+  (let* ((nodes (tiny-path-nodes path))
+         (len (tiny-path-length path))
+         (remaining-nodes (nthcdr n nodes)))
+    (values (when remaining-nodes
+              (%make-tiny-path :nodes remaining-nodes
+                               :absolute-p (tiny-path-absolute-p path)
+                               :length (- len n)))
             (subseq nodes 0 n))))
 
 (defun path-push (name path &optional dir)
