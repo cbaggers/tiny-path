b2bd0cc2870740e534a71de87f8f4d849bb0e968
cbaggers <chris.bagley@gmail.com>
2017-11-27 00:03:21 +0100

    tpath-dirs & tpath-files .. maybe better to make nicer pathnames api

--- pathnames.lisp
+++ pathnames.lisp
@@ -1,7 +1,5 @@
 (in-package :tiny-path.internals)
 
-
-
 ;;------------------------------------------------------------
 ;; Functions for moving between pathnames & tiny-paths
 
@@ -29,3 +27,24 @@
 ;;------------------------------------------------------------
 ;; Helper functions that just call down to uiop and wrap
 ;; results as tpaths
+
+(defun tpath-dirs (path)
+  (let* ((pname (etypecase path
+                  (tpath (tpath-to-pathname path))
+                  (string (tpath-to-pathname (make-tpath path)))
+                  (pathname path)))
+         (head (list nil))
+         (tail head))
+    (flet ((keep (x)
+             (let ((new (cons (tpath-from-pathname x) nil)))
+               (setf (cdr tail) new
+                     tail new))))
+      (uiop:collect-sub*directories pname t t #'keep)
+      (cddr head))))
+
+(defun tpath-files (path)
+  (let ((pname (etypecase path
+                 (tpath (tpath-to-pathname path))
+                 (string (tpath-to-pathname (make-tpath path)))
+                 (pathname path))))
+    (mapcar #'tpath-from-pathname (uiop:directory-files pname))))
